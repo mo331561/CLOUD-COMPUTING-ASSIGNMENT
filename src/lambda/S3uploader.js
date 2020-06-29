7982f43ab7c8248a172a41fae6f19796854d3da5
Mounika Bollavaram <55276428+mo331561@users.noreply.github.com>
2020-06-29 20:43:57 +0530

    first commit

--- src/lambda/S3uploader.js
+++ src/lambda/S3uploader.js
@@ -6,7 +6,7 @@ const AWS = require('aws-sdk')
 AWS.config.update({ region: process.env.REGION || 'ap-south-1' })
 const s3 = new AWS.S3();
 
-const uploadBucket = 'severless-s3-s3bucket-gewgtbvjabb8'   // << LOOK!
+const uploadBucket = 'upload-image-to-cloud-s3bucket-1pfkdiq7leq33'   // << LOOK!
 
 exports.handler = async (event) => {
   const result = await getUploadURL()
